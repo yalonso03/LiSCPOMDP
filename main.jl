325dc265a2746fe6071d96f6a9703040a1e9de45
mansurarief <marief@andrew.cmu.edu>
2023-08-15 16:13:03 -0700

    pushed new model

---
+++ main.jl
@@ -0,0 +1,40 @@
+using POMDPs
+using POMCPOW
+using POMDPModels
+using POMDPTools
+using BasicPOMCP
+
+include("LiPOMDP.jl")
+include("utils.jl")
+
+
+# POMCPOW Solver
+pomdp = LiPOMDP(obs_type="discrete")
+s0 = pomdp.init_state
+
+
+solver = POMCPOWSolver(tree_queries=100, next_action=next_action) #use our own random next_action function
+planner = solve(solver, pomdp)
+b0 = LiBelief([Normal(9, 0.2), Normal(3, 0.2), Normal(3, 0.2), Normal(9, 2.)], 0.0, 0.0, [false, false, false, false])
+
+a= action(planner, b0)
+ap, info = action_info(planner, b0, tree_in_info=true)
+tree = D3Tree(info[:tree], init_expand=1)
+inchrome(tree)
+
+
+pomcp_planner = solve(solver, pomdp)
+
+println("solved, going to simulate")
+pomdp = LiPOMDP(obs_type="continous")
+
+hr = HistoryRecorder(max_steps=11)
+hist = simulate(hr, pomdp, planner)
+rhist = simulate(hr, pomdp, RandomPolicy(pomdp))
+bhist = simulate(hr, pomdp, pomcp_planner)
+println("""
+    Cumulative Discounted Reward (for 1 simulation)
+        Random: $(eval_policy(pomdp, rhist)),
+        POMCPOW: $(eval_policy(pomdp, hist)),
+        POMCP: $(eval_policy(pomdp, bhist)).
+    """)
--- utils.jl
+++ utils.jl
@@ -1,6 +1,133 @@
-function sample_state_belief(b::POMCPOW.StateBelief{POWNodeBelief{State, Action, Any, LiPOMDP}})
+using Distributions
+using ParticleFilters
+
+
+function eval_policy(P, hist)
+    tot_r = 0.0
+    tot_emission = 0.0
+    for (s, a, b, o, r) in hist
+        #@show s, a, r
+        tot_r = r
+        tot_emission -= get_action_emission(P, a)
+    end
+
+    (s, a, b, o, r)  = hist[end]
+    tot_V = s.Vₜ
+
+    return (discounted_r = round(discounted_reward(hist), digits=2), tot_r=round(tot_r, digits=2), tot_emission=tot_emission, tot_V=tot_V)
+end
+function compute_chunk_boundaries(quantile_vols::Vector{Float64})
+    n = length(quantile_vols)
+    @assert n > 0 "quantile_vols must not be empty"
+
+    chunk_boundaries = Vector{Float64}(undef, n-1)
+    for i = 2:n
+        chunk_boundaries[i-1] = (quantile_vols[i] + quantile_vols[i-1]) / 2
+    end
+    return chunk_boundaries
+end
+
+function compute_chunk_probs(chunk_boundaries::Vector{Float64}, site_dist::Normal)
+    n = length(chunk_boundaries)
+    @assert n > 0 "chunk_boundaries must not be empty"
+
+    chunk_probs = Vector{Float64}(undef, n+1)
+
+    chunk_probs[1] = cdf(site_dist, chunk_boundaries[1])
+    for i = 2:n
+        chunk_probs[i] = cdf(site_dist, chunk_boundaries[i]) - cdf(site_dist, chunk_boundaries[i-1])
+    end
+    chunk_probs[n+1] = 1 - cdf(site_dist, chunk_boundaries[n])
+
+    return chunk_probs
+end
+
+function get_action_emission(P, a)
+    action_type = get_action_type(a)
+    action_number = get_site_number(a)
+    
+    # Subtract carbon emissions (if relevant)
+    r3 = (action_type == "MINE") ? P.CO2_emissions[action_number] * -1 : 0
+    return r3
+end
+
+function get_site_number(a::Action)
+    action_str = string(a)
+    len = length(action_str)
+    deposit_number = Int(action_str[len]) - 48  # -48 because Int() gives me the ascii code
+    return deposit_number
+end
+
+# I'm sure there's some builtin for this but I couldn't find it lol
+function splice(begin_index, end_index, str)
+    result = ""
+    for i = begin_index:end_index
+        result = result * str[i]
+    end
+    return result
+end
+
+function get_action_type(a::Action)
+    action_str = string(a)
+    len = length(action_str)
+    action_type = splice(1, len - 1, action_str)
+    return action_type
+end
+
+function str_to_action(s::String)
+    if s == "MINE1"
+        return MINE1
+    elseif s == "MINE2"
+        return MINE2
+    elseif s == "MINE3"
+        return MINE3
+    elseif s == "MINE4"
+        return MINE4
+    elseif s == "EXPLORE1"
+        return EXPLORE1
+    elseif s == "EXPLORE2"
+        return EXPLORE2
+    elseif s == "EXPLORE3"
+        return EXPLORE3
+    else
+        return EXPLORE4
+    end
+end
+
+function can_explore_here(a::Action, b::Any)
+    action_type = get_action_type(a)
+    site_number = get_site_number(a)
+
+    if action_type == "MINE" || isa(b, ParticleFilters.ParticleCollection{State})
+        return true
+    end
+
+    if isa(b, POMCPOW.StateBelief{POWNodeBelief{State, Action, Any, LiPOMDP}})
+        b = sample_state_belief(b)
+    end
+    
+    return !b.have_mined[site_number]        
+end
+
+
+function sample_state_belief(b::POMCPOW.StateBelief{POWNodeBelief{State, Action, Any, LiPOMDP}}, bin_beliefstate=true)
+    
     b_dist = b.sr_belief.dist
-    return rand(b_dist)[1]
+
+    function bin_elements(v)
+        return [x < 4 ? 2 : (x <= 8 ? 6 : 10) for x in v]
+    end
+    
+
+    if bin_beliefstate
+        new_state = deepcopy(b_dist.items)
+        for (i, state) in enumerate(b_dist.items)
+            b_dist.items[i][1].deposits = bin_elements(state[1].deposits)
+        end
+    end
+
+    out =  rand(b_dist)[1]
+    return out
 end
 
 function get_all_states(b::POMCPOW.StateBelief{POWNodeBelief{State, Action, Any, LiPOMDP}})
@@ -10,11 +137,13 @@ function get_all_states(b::POMCPOW.StateBelief{POWNodeBelief{State, Action, Any,
     return states
 end
 
-function compute_portion_below_threshold(P::LiPOMDP, b::Union{LiBelief, POMCPOW.StateBelief{POWNodeBelief{State, Action, Any, LiPOMDP}}}, idx::Int64)
+function compute_portion_below_threshold(P, b, idx::Int64)
     if isa(b, LiBelief)
         dist = b.deposit_dists[idx]
         portion_below_threshold = cdf(dist, P.min_n_units)
-    else            
+    elseif isa(b, ParticleCollection{State})
+        portion_below_threshold = 0.
+    else
         sampled_belief = get_all_states(b)
         n_rows = length(sampled_belief)
         num_below_threshold = sum(row[idx] < P.min_n_units for row in sampled_belief)
@@ -24,9 +153,9 @@ function compute_portion_below_threshold(P::LiPOMDP, b::Union{LiBelief, POMCPOW.
 end
 
 function next_action(
-    P::LiPOMDP, 
-    b::Union{LiBelief, State, POMCPOW.StateBelief{POWNodeBelief{State, Action, Any, LiPOMDP}}},
-    h::POWTreeObsNode{POWNodeBelief{State, Action, Any, LiPOMDP}}, 
+    P,
+    b,
+    h, 
     a::Union{Action, Nothing}=nothing, 
     arg::Union{Any, Nothing}=nothing,  
     b0::Union{LiBelief, Nothing}=nothing
